89bb872d3daa823e976a645c2cd4abfd7a79a982
Enrique Villa <enrique.villa97@hotmail.com>
2020-05-28 05:09:33 -0500

    Prep for heroku

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const DBURL = process.env.DBURL || "mongodb://localhost/musicAppdb";
 const PORT = process.env.PORT || "8080";
-const SECRET_TOKEN = process.env.PORT || "secret";
-module.exports = {DBURL, PORT, SECRET_TOKEN};
+const SECRET_TOKEN = process.env.SECRET_TOKEN;
+const SPOTIFY_KEY = process.env.SPOTIFY_KEY;
+module.exports = {DBURL, PORT, SECRET_TOKEN, SPOTIFY_KEY};
